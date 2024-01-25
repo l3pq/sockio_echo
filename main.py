ab5f770f9293012569360b6a6f060663f46d0968
Mikolaj Doroslawski <m.doroslawski@gmail.com>
2024-01-25 21:00:56 +0100

    .

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import socketio
-
-# Tworzenie serwera Socket.IO
 sio = socketio.Server(cors_allowed_origins='*')
 
-# Aplikacja WSGI
 app = socketio.WSGIApp(sio)
 
-# Obsługa zdarzenia połączenia
 @sio.event
 def connect(sid, environ):
     print('connect ', sid)
 
-# Obsługa zdarzenia wiadomości
 @sio.event
 def message(sid, data):
     print('message ', data)
     sio.emit('reply', data, to=sid)
 
-# Obsługa zdarzenia rozłączenia
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
-# Uruchomienie serwera
 if __name__ == '__main__':
     import eventlet
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
